103b601537383c9f850907a445211cbf40ad3763
jatin-rathee <jatinrathee2018@gmail.com>
2020-04-11 22:59:25 +0530

    added arrow functions

--- script.js
+++ script.js
@@ -94,6 +94,7 @@ console.log(c);
 
 /* ------------------- Strings ---------------------- */
 
+/*
 let firstName = 'Jatin';
 let lastName = 'Rathee';
 const yearOfBirth = 2000;
@@ -114,3 +115,32 @@ console.log(n.startsWith('J'));
 console.log(n.endsWith('th'));
 console.log(n.includes(' '));
 console.log(`${firstName} `.repeat(5));
+*/
+
+
+/* ------------------- Arrow functions ---------------------- */
+
+/*
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2020 - el;
+});
+console.log(ages5);
+
+// ES6
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Ages element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+age6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Ages element ${index + 1}: ${age}.`
+});
+
+console.log(ages6);
+*/
