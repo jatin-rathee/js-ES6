15ae1e9653dd624b9254382681832689b83a457c
jatin-rathee <jatinrathee2018@gmail.com>
2020-04-12 16:47:44 +0530

    classes

--- script.js
+++ script.js
@@ -435,15 +435,98 @@ function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
 
 var jatin = new SmithPerson('Jatin', 1990);
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
-*/
+
 
 // ES6
 function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
     this.firstName = firstName;
     this.lastName = lastName;
     this.yearOfBirth = yearOfBirth;
-    this.nationality = nationality;
+    this.nationality = nationality; 
 }
 
 var jatin = new SmithPerson('Jatin', 1990);
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish');
+*/
+
+
+/* ------------------- Maps ---------------------- */
+
+/*
+const question = new Map();
+question.set('question', 'What is official name of the latest major Javascript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer :D');
+question.set(false, 'Wrong, please try again!');
+
+console.log(question.get('question'));
+// console.log(question.size);
+
+// question.delete(4);
+
+if (question.has(4)) {
+    // question.delete(4);
+    // console.log('Answer 4 is here');
+}
+
+// question.clear();
+
+
+// question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`));
+
+for (let [key, value] of question.entries()) {
+    if (typeof(key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+
+console.log(question.get(ans === question.get('correct ')));
+*/
+
+
+/* ------------------- Classes ---------------------- */
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear - this.yearOfBirth;
+    console.log(age);
+}
+
+var jatin5 = new Person5('Jatin', 2000, 'developer');
+
+
+// ES6
+
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge() {
+        var age = new Date().getFullYear - this.yearOfBirth;
+        console.log(age);
+    }
+
+    // static method
+    static greeting() {
+        console.log('Hey there!');
+    }
+}
+
+const jatin6 = new Person6('Jatin', 2000, 'developer');
+
+Person6.greeting();
