aa36b7ee7a518d7248eb77c6ad9ce69ea122caa7
jatin-rathee <jatinrathee2018@gmail.com>
2020-04-12 14:48:06 +0530

    spread operator

--- script.js
+++ script.js
@@ -274,12 +274,11 @@ console.log(age, retirement);
 
 /* ------------------- Arrays ---------------------- */
 
-
+/*
 const boxes = document.querySelectorAll('.box');
 
 // ES5
 
-/*
 var boxesArr5 = Array.prototype.slice.call(boxes);
 
 boxesArr5.forEach(function(cur) {
@@ -312,7 +311,6 @@ for (const cur of boxesArr6) {
 
     cur.textContent = 'I changed to blue!';
 }
-*/
 
 
 // ES5
@@ -329,3 +327,40 @@ console.log(ages[full.indexOf(true)]);
 // ES6
 console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
+*/
+
+/* ------------------- Spread operator ---------------------- */
+
+/*
+function addFourAges (a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum = addFourAges(18, 30, 12, 21);
+console.log(sum);
+
+
+// ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);  
+
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+
+const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
+
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
+*/
+
